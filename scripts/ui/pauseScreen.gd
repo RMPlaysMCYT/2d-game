553db87ade80651799bc1b90ac01220064724643
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-04-29 21:50:05 +0800

    Add Pause Screen and It's Not Finished Yet

--- scripts/ui/pauseScreen.gd
+++ scripts/ui/pauseScreen.gd
@@ -1,5 +1,26 @@
 extends Control
 
+func _ready():
+	hide()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause_screen"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+func pause() -> void:
+	show()
+	get_tree().paused = true
+	
+func resume() -> void:
+	hide()
+	get_tree().paused = false
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://ui/Main.tscn")
+
+
+func _on_resume_butn_pressed() -> void:
+	resume()
--- scripts/ui/v_box_container.gd
+++ scripts/ui/v_box_container.gd
@@ -2,16 +2,20 @@ extends VBoxContainer
 
 @onready var mainMenu = $"."
 @onready var options = $"../OptionsPanel"
+@onready var PlayLevelScreen = $"../PlayLevelPanel"
 
 var audio_bus_id = AudioServer.get_bus_index("Audio")
 var sfx_bus_id = AudioServer.get_bus_index("SFX")
 
+
 func _ready()-> void:
 	mainMenu.show()
 	options.hide()
+	PlayLevelScreen.hide()
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Scene1.tscn")
+	PlayLevelScreen.visible = true
+	mainMenu.visible = false
 
 
 func _on_load_pressed() -> void:
@@ -26,21 +30,32 @@ func _on_options_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
-
-
-
+# OPTIONS SCREEN
+# OPTIONS SCREEN
 func _on_close_options_btn_pressed() -> void:
 	mainMenu.visible = true
 	options.visible = false
 	
 
-
 func _on_audio_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(audio_bus_id, linear_to_db(value))
 	
 
-
-
 func _on_sfx_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
+
+
+
+
+# PLAYSCREEN
+# PLAYSCREEN
+
+
+
+func _on_lv_l_1_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Scene1.tscn")
+
+
+func _on_back_btn_level_pressed() -> void:
+	PlayLevelScreen.visible = false
+	mainMenu.visible = true
