c2f3cc723c84a6131087eadc29a6c5160d45d6de
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-06-04 05:14:49 +0800

    Add Winning Parts and assets

--- scripts/finish_line_point.gd
+++ scripts/finish_line_point.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@export var win_screen_path: NodePath
+@onready var win_screen = get_node(win_screen_path)
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		print("Level Finished :D")
+	if body.is_in_group("player"):
+		win_screen.visible = true
+		win_screen.get_node("Panel/Score").set_value(body.Coins_Collected)
+		get_tree().paused = true
