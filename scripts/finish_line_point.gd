6c874b310933f169a28f3052fc7fc14308a1c64c
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-06-04 10:34:42 +0800

    Fixing on level complete issue

--- scripts/finish_line_point.gd
+++ scripts/finish_line_point.gd
@@ -1,10 +1,18 @@
 extends Area2D
 
-@export var win_screen_path: NodePath
-@onready var win_screen = get_node(win_screen_path)
+const WIN_SCREEN = preload("res://ui/LevelComplete.tscn")
+
+var triggered = false
+
+func _ready() -> void:
+	print("FinishLinePoint ready at position: ", global_position)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		win_screen.visible = true
-		win_screen.get_node("Panel/Score").set_value(body.Coins_Collected)
+	print("Something entered: ", body.name, " | is player: ", body.is_in_group("player"))
+	if body.is_in_group("player") and not triggered:
+		triggered = true
+		print("Coins: ", body.Coins_Collected)
+		var screen = WIN_SCREEN.instantiate()
+		get_tree().root.add_child(screen)
+		screen.get_node("Panel/Score").set_value(body.Coins_Collected)
 		get_tree().paused = true
