f833e990146b19d9d6af048bc844fb15fd9ecf36
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-06-05 10:34:33 +0800

    Fixing the finish point

--- scripts/finish_line_point.gd
+++ scripts/finish_line_point.gd
@@ -8,11 +8,10 @@ func _ready() -> void:
 	print("FinishLinePoint ready at position: ", global_position)
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Something entered: ", body.name, " | is player: ", body.is_in_group("player"))
 	if body.is_in_group("player") and not triggered:
 		triggered = true
-		print("Coins: ", body.Coins_Collected)
 		var screen = WIN_SCREEN.instantiate()
+		screen.process_mode = Node.PROCESS_MODE_ALWAYS  # add this
 		get_tree().root.add_child(screen)
 		screen.get_node("Panel/Score").set_value(body.Coins_Collected)
 		get_tree().paused = true
