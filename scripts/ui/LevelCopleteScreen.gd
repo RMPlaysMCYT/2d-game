67b8d14efc2c6a3cb107aa2027dd167a2f8209d9
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-06-05 21:01:01 +0800

    Fix assumingly the LevelComplete

--- scripts/ui/LevelCopleteScreen.gd
+++ scripts/ui/LevelCopleteScreen.gd
@@ -1,5 +1,8 @@
 extends Button
 
+@onready var finish_ui = $"../.."
+
 func _on_pressed() -> void:
+	get_tree().paused = false
+	finish_ui.queue_free()
 	get_tree().change_scene_to_file("res://ui/Main.tscn")
-	pass
