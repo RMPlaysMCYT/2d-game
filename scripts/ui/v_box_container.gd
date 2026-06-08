6c38972e309c02103d4fa663c333b1f2de7f4824
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-06-08 21:54:32 +0800

    add update

--- scripts/ui/v_box_container.gd
+++ scripts/ui/v_box_container.gd
@@ -103,16 +103,28 @@ func _on_level_2_btn_pressed() -> void:
 
 
 func _on_level_3_btn_pressed() -> void:
-	pass # Replace with function body.
+	if LevelSelectionCore.level1_completed == false:
+		null
+	if LevelSelectionCore.level2_completed == true:
+		get_tree().change_scene_to_file("res://scenes/Scene3.tscn")
 
 
 func _on_level_4_btn_pressed() -> void:
-	pass # Replace with function body.
+	if LevelSelectionCore.level1_completed == false:
+		null
+	if LevelSelectionCore.level3_completed == true:
+		get_tree().change_scene_to_file("res://scenes/Scene4.tscn")
 
 
 func _on_level_5_btn_pressed() -> void:
-	pass # Replace with function body.
+	if LevelSelectionCore.level4_completed == false:
+		null
+	if LevelSelectionCore.level4_completed == true:
+		get_tree().change_scene_to_file("res://scenes/Scene5.tscn")
 
 
 func _on_level_6_btn_pressed() -> void:
-	pass # Replace with function body.
+	if LevelSelectionCore.level5_completed == false:
+		null
+	if LevelSelectionCore.level5_completed == true:
+		get_tree().change_scene_to_file("res://scenes/Scene6.tscn")
