d7250e1a2bfce3bd2524ed81baad729b970dd59e
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-06-08 13:56:35 +0800

    Add Scene4 Layouts

--- models/enemy_1.gd
+++ models/enemy_1.gd
@@ -9,3 +9,15 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_timer_2_timeout() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_bomb_o_template_body_entered(body: Node2D) -> void:
+	if body.name == "player" and body.has_method("HarmedByAMob"):
+		body.HarmedByAMob()
+	timer.start()
+
+
+func _on_bomb_o_body_entered(body: Node2D) -> void:
+	if body.name == "player" and body.has_method("HarmedByAMob"):
+		body.HarmedByAMob()
+	timer.start()
